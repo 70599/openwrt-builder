3c192a91c547d31ffbd84ea50ae8a4465f3b3b78
saku <saku@DS220+>
2021-11-12 02:09:47 +0800

    refine cache workflow

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,21 +11,24 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >> feeds.conf
+echo 'src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf
 
 # udp2raw
 cd $GITHUB_WORKSPACE/openwrt/package
 git clone https://github.com/sensec/luci-app-udp2raw.git
 git clone https://github.com/sensec/openwrt-udp2raw.git
-cd luci-app-udp2raw/tools/po2lmo
-make
-sudo make install
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
 mv /tmp/OpenClash/luci-app-openclash $GITHUB_WORKSPACE/openwrt/package/
 rm -rf /tmp/OpenClash
+
+# Add po2lmo
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
