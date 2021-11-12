19117acb699f922a8447b821ccf808098eb16578
saku <saku@DS220+>
2021-11-12 13:38:06 +0800

    refine cache workflow

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,5 +17,5 @@ sed -i 's/192.168.1.1/10.10.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's|/bin/ash|/usr/bin/fish|g' package/base-files/files/etc/passwd
 
 # switch kernel version from 5.4 to 5.10
-sed -i 's|5.4|5.10|g' target/linux/x86/Makefile
+# sed -i 's|5.4|5.10|g' target/linux/x86/Makefile
 # sed -i 's|5.4|5.10|g' target/linux/rockchip/Makefile
