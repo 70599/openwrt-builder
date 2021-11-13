79cd7affb52a812b4981dcb9ad9bad1635d06945
saku <saku@DS220+>
2021-11-13 21:19:55 +0800

    update feeds and etc.

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,11 +37,12 @@ popd
 
 # Replace docker packages
 pushd feeds/packages/utils
-rm -rf containerd docker docker-compose dockerd
+rm -rf containerd docker docker-compose dockerd runc
 svn co https://github.com/openwrt/packages/trunk/utils/containerd
 svn co https://github.com/openwrt/packages/trunk/utils/docker
 svn co https://github.com/openwrt/packages/trunk/utils/docker-compose
 svn co https://github.com/openwrt/packages/trunk/utils/dockerd
+svn co https://github.com/openwrt/packages/trunk/utils/runc
 popd
 
 ./scripts/feeds update packages
