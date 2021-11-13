bef32d4f2a7c835af4095394c91068fb9d856ca5
automator <5929170+70599@users.noreply.github.com>
2021-11-13 09:41:10 +0800

    update config file(s)

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,49 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >> feeds.conf.default
-echo 'src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-echo 'src-git immortalwrt https://github.com/immortalwrt/luci' >> feeds.conf.default
-echo 'src-git community https://github.com/openwrt/packages' >> feeds.conf.default
-
-# remove doubled packages
-rm -rf package/lean/{luci-app-cpufreq,luci-app-verysync,verysync}
-
-mkdir -p $GITHUB_WORKSPACE/openwrt/package/additional
-pushd $GITHUB_WORKSPACE/openwrt/package/additional
-
-# udptools
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/luci-udptools
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/udp2raw
-svn co https://github.com/zcy85611/Openwrt-Package/trunk/udpspeeder-tunnel
-
-# OpenClash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
-mv /tmp/OpenClash/luci-app-openclash ./
-rm -rf /tmp/OpenClash
-
-popd
-
 # cpufreq
 ln -s feeds/immortalwrt/applications/luci-app-cpufreq package/feeds/luci/luci-app-cpufreq
 sed -i 's,1608,1800,g' feeds/immortalwrt/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's,2016,2208,g' feeds/immortalwrt/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's,1512,1608,g' feeds/immortalwrt/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
-
-# Pandownload
-pushd package/lean
-svn co https://github.com/immortalwrt/packages/trunk/net/pandownload-fake-server
-popd
-
-# Fix libssh
-pushd feeds/packages/libs
-rm -rf libssh
-svn co https://github.com/openwrt/packages/trunk/libs/libssh
-popd
-
-# po2lmo
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
