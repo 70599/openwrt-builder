0a391a0c13c7f5c27173f45ed59bf04e4437c8cb
root <root@R4S.lan>
2021-11-28 07:51:22 +0800

    test 21.02

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's|Lienol/openwrt-luci.git;main|Lienol/openwrt-luci.git;21.02|g' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >> feeds.conf.default
