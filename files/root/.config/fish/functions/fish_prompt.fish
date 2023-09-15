e7f321a2843e403a6819a1ddf965712462c3395e
Strainer4319 <5929170+70599@users.noreply.github.com>
2023-09-16 01:18:53 +0800

    Update fish_prompt.fish

--- files/root/.config/fish/functions/fish_prompt.fish
+++ files/root/.config/fish/functions/fish_prompt.fish
@@ -1,40 +1,42 @@
-set fish_color_command  normal
-set fish_color_cwd_root normal
-set fish_color_error    red
-set fish_color_param    a4a4a4
-set fish_greeting
-
-# function fish_prompt
-#	echo -n -s (set_color 888888) $USER (set_color normal) ' ' (prompt_pwd) ' ' (set_color green) '->' (set_color normal) ' '
-# end
+set -g fish_term24bit						1
+set -g fish_color_command  			normal
+set -g fish_color_cwd_root 			normal
+set -g fish_color_error    			red
+set -g fish_color_param    			foreground
+set -g fish_color_autosuggestion	brblack
+set -g fish_greeting
 
 function fish_prompt
-	set -l textcol    normal
-	set -l bgcol_user 4c515a
-	set -l bgcol      035f85
-	set -l arrowcol   035f85
-
-	# set_color $textcol -b $bgcol_user
-	# echo -n ' '$USER' '
-
-	set_color $textcol -b $bgcol_user
-	echo -n ' '(prompt_hostname)' '
-
-	set_color $textcol -b $bgcol
-	echo -n ' '(prompt_pwd)' '
-
-	set_color $arrowcol -b normal
-	echo -n ''
-
-	set_color normal -b normal
-	echo -n ' '
+	echo -ne -s (set_color brblue) '┌ ' (set_color brblack) (prompt_hostname) (set_color brblue) ' ' (prompt_pwd) '\n' (set_color brblue) '└─ ϟ' (set_color normal) ' '
 end
 
+# function fish_prompt
+# 	set -l textcol    normal
+# 	set -l bgcol_user 4c515a
+# 	set -l bgcol      035f85
+# 	set -l arrowcol   035f85
 
-# function fish_right_prompt
-#	set -l duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
-#	echo -n -s (set_color 888888) $duration (set_color normal)
+# 	# set_color $textcol -b $bgcol_user
+# 	# echo -n ' '$USER' '
+
+# 	set_color $textcol -b $bgcol_user
+# 	echo -n ' '(prompt_hostname)' '
+
+# 	set_color $textcol -b $bgcol
+# 	echo -n ' '(prompt_pwd)' '
+
+# 	set_color $arrowcol -b normal
+# 	echo -n ''
+
+# 	set_color normal -b normal
+# 	echo -n ' '
 # end
+
+
+function fish_right_prompt
+	set -l duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+	echo -n -s (set_color brblack) $duration (set_color normal)
+end
 
 function 'apk add'
 	apk add --no-cache $argv
@@ -42,6 +44,10 @@ end
 
 function dco
 	docker-compose $argv
+end
+
+function 'dco-reset'
+	docker-compose stop $argv[1] && docker-compose rm $argv[1] -f && docker-compose build --no-cache $argv[1] && docker-compose up $argv[1] -d
 end
 
 function dex
